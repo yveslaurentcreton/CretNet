908fcdb68cf3cb881f72ac5eb674434c7a8c6b7e
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2023-06-22 23:49:34 +0200

    Added initial version

---
+++ DotNetJet.FluentValidation.DependencyInjection/OptionsBuilderFluentValidationExtensions.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace DotNetJet.FluentValidation.DependencyInjection;
+
+public static class OptionsBuilderFluentValidationExtensions
+{
+    public static OptionsBuilder<TOptions> ValidateFluentValidation<TOptions>(
+        this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
+    {
+        optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(
+            provider => new FluentValidationOptions<TOptions>(optionsBuilder.Name, provider));
+        return optionsBuilder;
+    }
+}

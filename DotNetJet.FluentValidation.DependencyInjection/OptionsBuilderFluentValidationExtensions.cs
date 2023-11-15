632bc61c69672769a8e1ebaa5d76e2fcfdbf4ec3
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2023-11-15 17:50:29 +0100

    Added fallback to scope to search for validators

--- DotNetJet.FluentValidation.DependencyInjection/OptionsBuilderFluentValidationExtensions.cs
+++ DotNetJet.FluentValidation.DependencyInjection/OptionsBuilderFluentValidationExtensions.cs
@@ -9,8 +9,19 @@ public static class OptionsBuilderFluentValidationExtensions
     public static OptionsBuilder<TOptions> ValidateFluentValidation<TOptions>(
         this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
     {
-        optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(
-            provider => new FluentValidationOptions<TOptions>(optionsBuilder.Name, provider.GetRequiredService<IValidator<TOptions>>()));
+        optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(provider =>
+        {
+            var validator = provider.GetService<IValidator<TOptions>>();
+
+            if (validator is null)
+            {
+                var scope = provider.CreateScope();
+                validator = scope.ServiceProvider.GetRequiredService<IValidator<TOptions>>();
+            }
+
+            return new FluentValidationOptions<TOptions>(optionsBuilder.Name, validator);
+        });
+        
         return optionsBuilder;
     }
 }

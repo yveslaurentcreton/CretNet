5ca727711a7c7d2e73e23783ad25a4f5b2d106db
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2023-11-15 17:44:52 +0100

    undo change

--- DotNetJet.FluentValidation.DependencyInjection/OptionsBuilderFluentValidationExtensions.cs
+++ DotNetJet.FluentValidation.DependencyInjection/OptionsBuilderFluentValidationExtensions.cs
@@ -9,13 +9,8 @@ public static class OptionsBuilderFluentValidationExtensions
     public static OptionsBuilder<TOptions> ValidateFluentValidation<TOptions>(
         this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
     {
-        optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(provider =>
-        {
-            using var scope = provider.CreateScope();
-            var validator = scope.ServiceProvider.GetRequiredService<IValidator<TOptions>>();
-            return new FluentValidationOptions<TOptions>(optionsBuilder.Name, validator);
-        });
-
+        optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(
+            provider => new FluentValidationOptions<TOptions>(optionsBuilder.Name, provider.GetRequiredService<IValidator<TOptions>>()));
         return optionsBuilder;
     }
 }

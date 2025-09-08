3f4561701f3cb6a023393f12c4180d25fe740fa7
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2025-09-08 18:35:19 +0200

    feat: Promote the CnpEntityView as the only way to display the entity information.

--- src/CretNet.Platform.Blazor/Components/CnpEntityView.razor.cs
+++ src/CretNet.Platform.Blazor/Components/CnpEntityView.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Fluxor;
 using CretNet.Platform.Blazor.Services;
 using CretNet.Platform.Data;
+using CretNet.Platform.Fluxor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 
@@ -8,11 +9,12 @@ namespace CretNet.Platform.Blazor.Components;
 
 public partial class CnpEntityView<TEntity, TId>
     where TEntity : class, IIdentity<TId>
-    where TId : notnull
+    where TId : struct
 {
     private Guid _id = Guid.NewGuid();
 
-    [Parameter, EditorRequired] public TEntity Entity { get; set; } = default!;
+    [Parameter] public TId? EntityId { get; set; }
+    [Parameter] public TEntity? Entity { get; set; }
     [Parameter] public Icon? Icon { get; set; }
     [Parameter, EditorRequired] public RenderFragment<TEntity> ChildContent { get; set; } = default!;
     
@@ -20,6 +22,35 @@ public partial class CnpEntityView<TEntity, TId>
     [Inject] public IEntityDefinition<TEntity, TId> EntityDefinition { get; set; } = default!;
     
     protected bool ShouldShowNavigation => EntityDefinition.HasNavigationAction;
+    
+    public override Task SetParametersAsync(ParameterView parameters)
+    {
+        var ret = base.SetParametersAsync(parameters);
+
+        if (parameters.TryGetValue<TId?>(nameof(EntityId), out var entityId))
+        {
+            if (entityId is null)
+                return ret;
+            
+            InvokeAsync(async () =>
+            {
+                var fetchAction = EntityDefinition.CreateFetchAction(entityId.Value);
+                var fetchedEntity = await Dispatcher.DispatchAsync(fetchAction);
+                Entity = fetchedEntity;
+                
+                StateHasChanged();
+            });
+        }
+
+        if (parameters.TryGetValue<TEntity?>(nameof(Entity), out var newEntity))
+        {
+            Entity = newEntity;
+            
+            InvokeAsync(StateHasChanged);
+        }
+
+        return ret;
+    }
     
     protected void Navigate()
     {
